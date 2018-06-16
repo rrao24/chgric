8a4ebfbcf623b8a35942428ed3ecf829bc1a74f9
rrao24 <rao.ram5718@gmail.com>
2018-06-16 00:24:49 -0400

    line graph functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,13 @@
 $( document ).ready(function() {
+	/** Global Variables **/
+
+	//Hold all game objects to be used by any and all functions
+	var games = [];
+
+	//Hold mapping of player name to associated array of scores
+	//used by graphing function
+	var playerToScoreArrayDict = {};
+
 	//Get data from csv
     $( "#target" ).load("https://rrao24.github.io/chgric/gric.csv", function() {
     	//Parse csv
@@ -8,7 +17,6 @@ $( document ).ready(function() {
 				
 				results = results.data;
 				var currentGame = 1;
-				var games = [];
 				var game = {};
 
 				//Iterate through all lines in excel sheet
@@ -54,7 +62,60 @@ $( document ).ready(function() {
 				$('#target').append('<br><br>');
 				$('#target').append(JSON.stringify(games));
 				console.log(games);
+
+				//Populate player to score dictionary
+				for (var l = 0; l < games.length; l++) {
+					var tempStats = games[l].players;
+					for (var m = 0; m < tempStats.length; m++) {
+						var currName = tempStats[m].name;
+						var currScore = tempStats[m].total;
+						if (!playerToScoreArrayDict[currName]) {
+							playerToScoreArrayDict[currName] = [];
+						}
+						playerToScoreArrayDict[currName].push(currScore);
+					}
+				}
+
+				//Create interactive HTML option for user
+				var playerOptionStr = 'Display Line Graph For: <select id="playerOption" name="players">';
+				var playerNames = Object.keys(playerToScoreArrayDict);
+				for (var p = 0; p < playerNames.length; p++) {
+					playerOptionStr += '<option value="' + playerNames[p] + '">' + playerNames[p] + '</option>';
+				}
+				playerOptionStr += '</select>';
+				$('#playerSelect').append(playerOptionStr);
 			}
+		});
+    });
+
+    //Generate associated line graph for scores upon user clicking a player's name
+    $(document).on('change', '#playerOption', function(e) {
+    	e.stopPropagation();
+    	e.preventDefault();
+    	var selectedPlayer = $('#playerOption').val();
+    	var ctx = document.getElementById('gricVisual').getContext('2d');
+		var xLabels = [];
+		for (var q = 0; q<playerToScoreArrayDict[selectedPlayer].length; q++) {
+			if ((q+1) % 4 == 1) {
+				xLabels.push(q+1);
+			} else {
+				xLabels.push("");
+			}
+		}
+
+		var chart = new Chart(ctx, {
+		    type: 'line',
+		    data: {
+		        labels: xLabels,
+		        datasets: [{
+		            label: "Score",
+		            fill: false,
+		            backgroundColor: 'rgb(255, 99, 132)',
+		            borderColor: 'rgb(255, 99, 132)',
+		            data: playerToScoreArrayDict[selectedPlayer],
+		        }]
+		    },
+		    options: {}
 		});
     });
 });
